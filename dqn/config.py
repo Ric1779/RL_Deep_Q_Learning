913b6da98ce5c8bafed89c018074864c90f5c09f
Richards <richards.1779@gmail.com>
2023-02-27 21:29:42 +0100

    Pong added

--- dqn/config.py
+++ dqn/config.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from agents.base_agent import BaseAgent
 from agents.pong.agent import PongAgent
+from agents.cartpole.agent import CartPoleAgent
 
-agent_map: Dict[str, BaseAgent] = {"pong": PongAgent}
+agent_map: Dict[str, BaseAgent] = {"pong": PongAgent, "cartpole": CartPoleAgent}
